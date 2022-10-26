d02aab8c889ff7a4702f13f6227038de4e00f695
badvell <badvell@mail.ru>
2022-10-26 16:09:17 +0200

    fixed

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -11,7 +11,7 @@ export const ProjectDisplay = () => {
   return (
     <div className='project'>
       <h1>{project.name}</h1>
-      <img src={project.image} />
+      <img src={project.image} alt='randomtext' />
       <p>
         <b>Skills:</b> {project.skills}
       </p>
